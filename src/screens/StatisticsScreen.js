1559aaeaccf80e363214e16eddad68bdd44c67ed
lazaroadrian <lazaroadrian0803@gmail.com>
2025-09-07 13:10:30 -0400

    sadf

--- src/screens/StatisticsScreen.js
+++ src/screens/StatisticsScreen.js
@@ -1849,7 +1849,7 @@ const StatisticsContent = ({ navigation, onModeVisibilityChange }) => {
               ) : (
                 <Text style={[styles.excelHeaderCell, { width: 60 }]}>Ganancia</Text>
               )}
-              <Text style={[styles.excelHeaderCell, { width: 60 }]}>Pagado</Text>
+              <Text style={[styles.excelHeaderCell, { width: 60 }]}>{userRole === 'collector' ? 'Premio' : 'Pagado'}</Text>
               {userRole === 'collector' ? (
                 <>
                   <Text style={[styles.excelHeaderCell, { width: 60 }]}>Bal. Listero</Text>
